bce5e55e4e91f91ef7c754b6e22a7787a326fc3f
Markusovich <simon.markus9@gmail.com>
2021-01-27 23:13:45 -0600

    changes

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from flask import Flask, render_template, request, redirect
 from IPython.display import HTML 
 app = Flask(__name__)
 
+# This function returns a dataframe of the most recent covid data
+
 def getData():
     # Importing data set to client variable
     client = Socrata("data.cdc.gov", None)
-    results = client.get("9mfq-cb36", limit=22000)
+    results = client.get("9mfq-cb36", limit=23000)
 
     # Convert to pandas DataFrame so we can work with the dataset
     results_df = pd.DataFrame.from_records(results)
@@ -16,7 +18,7 @@ def getData():
     # Removing unhelpful columns
     results_df = results_df.drop(['consent_cases', 'consent_deaths', 'created_at'], 1)
     # Removing rows with NaN values
-    results_df = results_df.dropna()
+    #results_df = results_df.dropna()
 
     # Converting date column from string to datetime object
     results_df['submission_date'] = pd.to_datetime(results_df['submission_date'])
@@ -35,23 +37,32 @@ def getData():
 def homefunc():
     return render_template('home.html')
 
-@app.route('/exportdata', methods=['GET', 'POST'])
+@app.route('/datasearch', methods=['GET', 'POST'])
 def datafunc():
 
     if request.method == 'POST':
-        if request.form['state'] != "None":
-            state = request.form['state']
+
+        state = request.form['state']
+        if state != "None":
             data = getData()
             data = data[data['state'] == state]
         else:
             data = getData()
+            pass
+
+        check = request.form.get('defaultCheck1')
+        print(check)
+        if check == "yes":
+            data = data.groupby(['state']).first().reset_index()
+        else:
+            pass
 
         data.reset_index(drop=True, inplace=True)
 
-        return render_template('exportdata.html', data=data.to_html())
+        return render_template('datasearch.html', data=data.to_html())
     else:
-        return render_template('exportdata.html')
+        return render_template('datasearch.html')
 
-@app.route('/search')
+@app.route('/datavisualization')
 def searchfunc():
-    return render_template('search.html')
+    return render_template('datavisualization.html')
